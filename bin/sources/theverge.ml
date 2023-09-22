087b84db1833d36326f366823244fd918b461755
arthurmelton <git-zl9limki@dontrackme.com>
2023-09-21 21:23:26 -0400

    remove the verge sidebar

--- bin/sources/theverge.ml
+++ bin/sources/theverge.ml
@@ -19,46 +19,49 @@ let get_content input =
       .duet--article--article-body-component"
   |> to_list
   |> List.map (fun x ->
-    x
-    $$ "a"
-    |> to_list
-    |> List.iter (fun y ->
-      let href = R.attribute "href" y in
-      if String.starts_with ~prefix:"/" href
-      then set_attribute "href" ("/theverge" ^ href) y);
-    let p = x $? "p" in
-    if Option.is_some p
+    if Option.is_none (x $? ".duet--article--sidebar")
     then (
-      let p = Option.get p in
-      p |> delete_attribute "class";
-      p |> to_string)
-    else (
-      let img = x $? "img[srcset]" in
-      if Option.is_some img
+      x
+      $$ "a"
+      |> to_list
+      |> List.iter (fun y ->
+        let href = R.attribute "href" y in
+        if String.starts_with ~prefix:"/" href
+        then set_attribute "href" ("/theverge" ^ href) y);
+      let p = x $? "> p" in
+      if Option.is_some p
       then (
-        let img = Option.get img in
-        let img_src =
-          "/proxy/"
-          ^ (R.attribute "srcset" img
-             |> String.split_on_char ' '
-             |> List.rev
-             |> fun y -> List.nth y 1)
-        in
-        [ "decoding"; "data-nimg"; "style"; "srcset"; "loading"; "sizes" ]
-        |> List.iter (fun y -> delete_attribute y img);
-        set_attribute "src" img_src img;
-        let caption = x $$ ".duet--media--caption > *" |> to_list in
-        caption |> List.iter (delete_attribute "class");
-        (img |> to_string) ^ (caption |> List.map to_string |> String.concat ""))
+        let p = Option.get p in
+        p |> delete_attribute "class";
+        p |> to_string)
       else (
-        let ul = x $? "ul" in
-        if Option.is_some ul
+        let img = x $? "img[srcset]" in
+        if Option.is_some img
         then (
-          let ul = Option.get ul in
-          delete_attribute "class" ul;
-          ul $$ "li" |> to_list |> List.iter (delete_attribute "class");
-          ul |> to_string)
-        else "")))
+          let img = Option.get img in
+          let img_src =
+            "/proxy/"
+            ^ (R.attribute "srcset" img
+               |> String.split_on_char ' '
+               |> List.rev
+               |> fun y -> List.nth y 1)
+          in
+          [ "decoding"; "data-nimg"; "style"; "srcset"; "loading"; "sizes" ]
+          |> List.iter (fun y -> delete_attribute y img);
+          set_attribute "src" img_src img;
+          let caption = x $$ ".duet--media--caption > *" |> to_list in
+          caption |> List.iter (delete_attribute "class");
+          (img |> to_string) ^ (caption |> List.map to_string |> String.concat ""))
+        else (
+          let ul = x $? "ul" in
+          if Option.is_some ul
+          then (
+            let ul = Option.get ul in
+            delete_attribute "class" ul;
+            ul $$ "li" |> to_list |> List.iter (delete_attribute "class");
+            ul |> to_string)
+          else "")))
+    else "")
   |> String.concat ""
 ;;
 
